b9ea40bf1a831d8665af42ddbc853a3acf7cf6e4
MaxHuw <m.huw.graham@gmail.com>
2019-04-11 14:46:41 -0400

    Map example displays.

--- frontend/src/components/_map.js
+++ frontend/src/components/_map.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class Map extends React.Component {
   componentDidMount(){
@@ -7,7 +8,27 @@ class Map extends React.Component {
 
   render() {
     return (
-        <p>This is where the Map will go.</p>
+      <LeafletMap
+        center={[50, 10]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[50, 10]}>
+          <Popup>
+            Popup for any custom information.
+          </Popup>
+        </Marker>
+      </LeafletMap>
     );
   }
 }
